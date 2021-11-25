7a3595c4cd835ff3c2227829ab408033879e85bc
ccarabine <ccarabine12@gmail.com>
2021-11-25 14:57:03 +0000

    add more files

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    //modal initialization
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+    
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+});
